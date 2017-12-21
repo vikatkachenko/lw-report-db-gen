7d29caaad4b1f719c69c4538cba15e63c4b0f68b
vikatkachenko <34066692+vikatkachenko@users.noreply.github.com>
2017-12-21 18:03:53 +0100

    Change project.clj

--- project.clj
+++ project.clj
@@ -3,10 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/test.check "0.9.0"]
-                 [orchestra "2017.07.04-1"]
-                 [com.rpl/specter "1.0.4"]
                  [org.clojure/data.csv "0.1.4"]
                  [clj-time "0.14.2"]]
   :main ^:skip-aot lw-report-db-gen.core
